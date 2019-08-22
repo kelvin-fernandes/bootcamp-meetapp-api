9821995e6b16e66d502f2d4a26fc0b14a80ecc31
kelvin-fernandes <sokelvinfernandes@gmail.com>
2019-08-22 10:27:04 -0400

    added update in user controller

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -34,6 +34,52 @@ class UserController {
             email
         });
     }
+
+    async update(req, res) {
+        const schema = Yup.object().shape({
+            name: Yup.string(),
+            email: Yup.string().email(),
+            oldPassword: Yup.string(),
+            password: Yup.string()
+                .min(4)
+                .when('oldPassword', (oldPassword, field) =>
+                    oldPassword ? field.required() : field
+                ),
+            confirmPassword: Yup.string().when('password', (password, field) =>
+                password ? field.required().oneOf([Yup.ref('password')]) : field
+            )
+        });
+
+        if (!(await schema.isValid(req.body))) {
+            schema.validate(req.body).catch(err => {
+                return res.status(400).json({
+                    error: err.errors ? err.errors[0] : 'Validation failed'
+                });
+            });
+        }
+
+        const { email, oldPassword } = req.body;
+
+        const user = await User.findByPk(req.userId);
+
+        if (email && email !== user.email) {
+            const userExists = await User.findOne({
+                where: { email: req.body.email }
+            });
+
+            if (userExists) {
+                return res.status(400).json({ error: 'User already exists' });
+            }
+        }
+
+        if (oldPassword && !(await user.checkPassword(oldPassword))) {
+            return res.status(400).json({ error: 'Password does not match' });
+        }
+
+        const { id, name } = await user.update(req.body);
+
+        return res.json({ id, name, email });
+    }
 }
 
 export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import authMiddleware from './app/middlewares/auth';
 
+import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
+routes.post('/sessions', SessionController.store);
 routes.post('/users', UserController.store);
+
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 export default routes;
