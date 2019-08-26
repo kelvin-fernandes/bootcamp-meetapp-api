23421d43e9e14fd0b4fef9eacccec212fffaf59f
kelvin-fernandes <sokelvinfernandes@gmail.com>
2019-08-25 23:10:26 -0400

    updated database logging turning off

--- src/config/database.js
+++ src/config/database.js
@@ -4,6 +4,7 @@ module.exports = {
     username: 'postgres',
     password: 'docker',
     database: 'meetapp',
+    logging: false,
     define: {
         timestamps: true,
         underscored: true,
